ecad1f01bbc8dfd3bdebab27e02edfea1d3a48e7
flycash <ming_flycash@qq.com>
2019-05-07 23:21:28 +0800

    init

---
+++ src/main/java/cn/com/flycash/App.java
@@ -0,0 +1,13 @@
+package cn.com.flycash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/cn/com/flycash/stupidmock/StupidMock.java
@@ -0,0 +1,8 @@
+package cn.com.flycash.stupidmock;
+
+public class StupidMock {
+
+    public static <T> T mock(Class<T> tClass) {
+        return null;
+    }
+}
